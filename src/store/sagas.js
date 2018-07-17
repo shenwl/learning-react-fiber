63f86637aeda6fa20d210c8413e17270aa43957e
shenwl <shenweilun96@gmail.com>
2018-07-17 21:04:12 +0800

    添加异常处理

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -4,9 +4,13 @@ import { initListAction } from './actionCreators'
 import axios from 'axios'
 
 function* getInitList() {
-  const res = yield axios.get('http://0.0.0.0:3001/api/list')
-  const action = initListAction(res.data)
-  yield put(action)
+  try {
+    const res = yield axios.get('http://0.0.0.0:3001/api/list')
+    const action = initListAction(res.data)
+    yield put(action)
+  } catch(e) {
+    console.log('getInitList网络请求失败')
+  }
 }
 
 // 要用generator
