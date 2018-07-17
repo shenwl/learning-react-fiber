a5256c61d5bfeea404fee8e51ab5729e95bc3cd4
shenwl <shenweilun96@gmail.com>
2018-07-17 20:36:52 +0800

    apply sagaMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
+import createSagaMiddleware from 'redux-saga'
+
+const sagaMiddleWare = createSagaMiddleware()
 
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? 
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
 
 const enhancer = composeEnhancers(
-  applyMiddleware(),
+  applyMiddleware(sagaMiddleWare),
 )
 
 const store = createStore(reducer, enhancer)
