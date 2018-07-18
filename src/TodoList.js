318f64ba60352761de174b6f58a45db8f48d2f2f
shenwl <shenweilun96@gmail.com>
2018-07-18 20:15:14 +0800

    改用无状态组件，增强性能

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,35 +2,33 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 
-class TodoList extends Component {
-  render() {
-    const { inputValue, handleClick, handleInputChange, handleDeleteItem, list } = this.props
-    return (
+const TodoList = (props) => {
+  const { inputValue, handleClick, handleInputChange, handleDeleteItem, list } = props
+  return (
+    <div>
       <div>
-        <div>
-          <input 
-            value={inputValue}
-            onChange = {this.props.handleInputChange.bind(this)}
-          />
-          <button onClick={handleClick.bind(this)}>提交</button>
-        </div>
-        <ul>
-          {
-            list.map((item, index) => {
-              return (
-                <li 
-                  key={item}
-                  onClick={handleDeleteItem.bind(this, index)}
-                >
-                  {item}
-                </li>
-              )
-            })
-          }
-        </ul>
+        <input 
+          value={inputValue}
+          onChange = {handleInputChange.bind(this)}
+        />
+        <button onClick={handleClick.bind(this)}>提交</button>
       </div>
-    )
-  }
+      <ul>
+        {
+          list.map((item, index) => {
+            return (
+              <li 
+                key={item}
+                onClick={handleDeleteItem.bind(this, index)}
+              >
+                {item}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
